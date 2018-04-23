0fc8ff241d2ebe0599938f74e4b49c99cd7b70b4
William Xavier Sousa <muzkle@gmail.com>
2018-04-22 23:16:22 -0300

    simples change

--- bot.js
+++ bot.js
@@ -74,4 +74,5 @@ client.on('message', msg => {
 // }
 
 //process.env.BOT_TOKEN | 
-client.login('NDM3NjM0MDY5NzI4MTk4NjU3.Db46YQ.ATf6OFoYbYMzjILfh7RK5eZxyNY');
+// NDM3NjM0MDY5NzI4MTk4NjU3.Db46YQ.ATf6OFoYbYMzjILfh7RK5eZxyNY
+client.login(process.env.BOT_TOKEN);
